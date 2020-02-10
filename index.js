42d60cc7d31c7392c94fcab7765f31e16dcd1f5e
Vincent Sanders <vincesanders@hotmail.com>
2020-02-10 14:35:22 -0600

    Added get request for all users. Added boilerplate for other endpoints.

--- index.js
+++ index.js
@@ -7,4 +7,34 @@ const server = express();
 
 server.use(express.json());
 
+//get array of all user objects
+server.get('/api/users', (req, res) => {
+    database.find().then(users => {
+        res.status(200).json(users);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: 'oops' });
+    });
+});
+
+//get user object by id
+server.get('/api/users/:id', (req, res) => {
+
+});
+
+//Updates the user with the specified id using data from the request body. Returns the modified document, NOT the original.
+server.put('/api/users/:id', (req, res) => {
+
+});
+
+//Creates a user using the information sent inside the request body.
+server.post('/api/users', (req, res) => {
+
+});
+
+//Removes the user with the specified id and returns the deleted user.
+server.delete('/api/users/:id', (req, res) => {
+
+});
+
 server.listen(5000, () => console.log('Server is listening on port 5000.'))
