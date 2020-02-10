cfc561bf5842e62298aaf615f9011eee17a107dc
Vincent Sanders <vincesanders@hotmail.com>
2020-02-10 14:20:00 -0600

    Added basic server template to index.js.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // implement your API here
+const express = require('express');
+
+const database = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.listen(5000, () => console.log('Server is listening on port 5000.'))
