1d31a7b72222de345cf706e250d2900fcd8c21c1
Vincent Sanders <vincesanders@hotmail.com>
2020-02-10 15:41:36 -0600

    Added validation for put requests.

--- index.js
+++ index.js
@@ -34,14 +34,21 @@ server.get('/api/users/:id', (req, res) => {
 //Updates the user with the specified id using data from the request body. Returns the modified document, NOT the original.
 server.put('/api/users/:id', (req, res) => {
     database.update(req.params.id, req.body).then(userID => {
-        if (!user) {
+        if (!userID) {
             res.status(404).json({ message: "The user with the specified ID does not exist." });
+        } else if (!req.body.name || !req.body.bio) {
+            res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
         } else {
-            res.status(200).json(userID);
+            database.findById(userID).then(user => {
+                res.status(200).json(user);
+            }).catch(err => {
+                console.log(err);
+                res.status(500).json({ errorMessage: "The user information could not be retrieved." });
+            });
         }
     }).catch(err => {
         console.log(err);
-        res.status(500).json({ errorMessage: 'oops' });
+        res.status(500).json({ errorMessage: "The user information could not be modified." });
     });
 });
 
